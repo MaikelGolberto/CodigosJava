8cff5cbde37e721a85e9439e129e0b5b322a6d9d
Maikel Golberto <92800007+MaikelGolberto@users.noreply.github.com>
2022-04-11 00:37:05 -0300

    Update ProgramV2.java

--- TratamentoDeExcecoes-Exercicio/src/application/ProgramV2.java
+++ TratamentoDeExcecoes-Exercicio/src/application/ProgramV2.java
@@ -1,4 +1,4 @@
-//Versãqo simplificada, se acontecer o erro o programa encerra!
+//VersÃ£qo simplificada, se acontecer o erro o programa encerra!
 
 package application;
 
@@ -15,29 +15,35 @@ public class ProgramV2 {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		try {
-			System.out.println("Entre com os dados da conta: ");
-			System.out.print("Número: ");
-			int number = sc.nextInt();
-			sc.nextLine();
-			System.out.print("Nome: ");
-			String name = sc.nextLine();
-			System.out.print("Saldo inicial: ");
-			double balance = sc.nextDouble();
-			System.out.print("Limite de saque: ");
-			double withdrawLimit = sc.nextDouble();
-			
-			Account acc = new Account(number, name, balance, withdrawLimit);
-			
-			System.out.println();
-			System.out.print("Entre com o valor para saque: ");
-			double amount = sc.nextDouble();
+		System.out.println("Entre com os dados da conta: ");
+		System.out.print("NÃºmero: ");
+		int number = sc.nextInt();
+		sc.nextLine();
+		System.out.print("Nome: ");
+		String name = sc.nextLine();
+		System.out.print("Saldo inicial: ");
+		double balance = sc.nextDouble();
+		System.out.print("Limite de saque: ");
+		double withdrawLimit = sc.nextDouble();
 		
+		Account acc = new Account(number, name, balance, withdrawLimit);
+		
+		System.out.println();
+		System.out.print("Entre com o valor para saque: ");
+		double amount = sc.nextDouble();
+		try {
 			acc.withdraw(amount);
 			System.out.println("Saque realizado com sucesso!\nSaldo atual: " + String.format("%.2f", acc.getBalance()));	
 			
 		} catch (DomainException e) {
 			System.out.println(e.getMessage());
+			Account acc1 = new Account(number, name, balance, withdrawLimit);
+			System.out.println("\nAlgo deu errado, tente novamente:\n");
+			System.out.println("Saldo disponÃ­vel: " + String.format("%.2f", acc1.getBalance()) + " | Limite de saque: " + String.format("%.2f", acc1.getWithdrawLimit()));
+			System.out.print("Entre com o valor para saque: ");
+			double amount1 = sc.nextDouble();
+			acc1.withdraw(amount1);
+			System.out.println("Saque realizado com sucesso!\nSaldo atual: " + String.format("%.2f", acc1.getBalance()));
 		}
 			
 		sc.close();
